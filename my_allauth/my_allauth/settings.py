b96e77bc227237c7e60b01f507bcea9eb1cb6452
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-05-06 13:24:45 +0200

    Login via Github and redirecting

--- my_allauth/my_allauth/settings.py
+++ my_allauth/my_allauth/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'my_allauth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -142,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL ='/'
